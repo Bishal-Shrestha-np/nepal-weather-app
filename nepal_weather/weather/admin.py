e07ac37261936692cd05423a2d8bc1248986bac5
Bishal Shrestha <shrestha.np.bishal@gmail.com>
2025-09-28 11:22:05 +0545

    Fixed errors

--- nepal_weather/weather/admin.py
+++ nepal_weather/weather/admin.py
@@ -10,3 +10,16 @@ class PopularCityAdmin(admin.ModelAdmin):
     list_editable = ['is_active']
     ordering = ['-search_count']
 
+@admin.register(WeatherData)
+class WeatherDataAdmin(admin.ModelAdmin):
+    list_display = ['city', 'temperature', 'description', 'updated_at']
+    list_filter = ['country', 'updated_at']
+    search_fields = ['city']
+    readonly_fields = ['created_at', 'updated_at']
+
+@admin.register(WeatherSearch)
+class WeatherSearchAdmin(admin.ModelAdmin):
+    list_display = ['city', 'ip_address', 'search_time', 'success']
+    list_filter = ['success', 'search_time']
+    search_fields = ['city', 'ip_address']
+    readonly_fields = ['search_time']
